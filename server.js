cfa0292e3ba2c2d9ddd9ecabfc0b42871cec35cc
vamsikrishna <vamsichowdarychowdary812@gmail.com>
2026-03-31 16:10:56 +0530

    fix static path

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const upload = multer({ storage });
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // =============================================
